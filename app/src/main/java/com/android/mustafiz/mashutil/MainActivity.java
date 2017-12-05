ad1bedc20fabab2c08140cc9acb414bf1b2ef514
mustafiz012 <mustafiz012@outlook.com>
2017-12-05 15:42:43 +0600

    Login hoitiache

--- app/src/main/java/com/android/mustafiz/mashutil/MainActivity.java
+++ app/src/main/java/com/android/mustafiz/mashutil/MainActivity.java
@@ -1,13 +1,83 @@
 package com.android.mustafiz.mashutil;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    //https://www.youtube.com/watch?v=KjBNFWKNMOY
+    CallbackManager callbackManager;
+    TextView profileName;
+    ProgressDialog progressDialog;
+    LoginButton loginButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        callbackManager = CallbackManager.Factory.create();
+        profileName = findViewById(R.id.profileName);
+        loginButton = findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("public_profile", "email", "user_birthday", "user_friends"));
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                progressDialog = new ProgressDialog(MainActivity.this);
+                progressDialog.setMessage("Getting user info...");
+                progressDialog.show();
+
+                String accessToken = loginResult.getAccessToken().getToken();
+                GraphRequest graphRequest = GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response) {
+                        progressDialog.dismiss();
+                        Log.i(TAG, "onCompleted: " + response.getJSONObject().toString());
+                        try {
+                            profileName.setText(object.getString("name"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                Bundle params = new Bundle();
+                params.putString("friends", "id,email,birthday,friends");
+                graphRequest.setParameters(params);
+                graphRequest.executeAsync();
+            }
+
+            @Override
+            public void onCancel() {
+                Log.i(TAG, "onCancel: ");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.i(TAG, "onError: ");
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
